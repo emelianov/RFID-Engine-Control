a7c965188eecc82a41d2197a389ddd5a4a9c4da1
Alexander Emelianov <a.m.emelianov@gmail.com>
2017-03-27 21:04:33 +0500

    Comments added

--- README.md
+++ README.md
@@ -5,3 +5,9 @@
 * [Adafruit GFX Library](https://github.com/adafruit/Adafruit-GFX-Library)
 * [MFRC522](https://github.com/miguelbalboa/rfid)
 * [EEPROM2.h](http://freeduino.ru/arduino/sample_EEPROM.html)
+### Hardware
+* [Relays shield](http://m.intl.taobao.com/detail/detail.html?spm=0.0.0.0&id=521493640182)
+Relay pins: 10, 11, 12, 13
+* RC522 reader
+* I2C 128x64 display
+* Arduino UNO board
--- main/main.ino
+++ main/main.ino
@@ -12,21 +12,41 @@
 #include <Adafruit_SSD1306.h> // Diaplay library
 #include <avr/wdt.h>          // WatchDog Timer library
 
+
+// Shutdown pin. Shuold be connected to ground to initiate action.
 #define PIN2          A5
-#define RF_PIN        10
-#define RF_RESET      11
-#define RELAY1        A0
-#define RELAY2        A1
-#define LCD_ID        0x3C
-#define CARD_MAX      5
-#define IGNITION      (digitalRead(RELAY1)==LOW)
-#define START         start
-#define PIN1_HIGH     (digitalRead(PIN1)==LOW)
-#define PIN2_HIGH     (digitalRead(PIN2)==LOW)
-#define TRYS_MAX      5
-#define EEPROM_CARDS  0
-#define OLED_RESET    4
+// Card program pin. Should be connected to ground to initiate action
 #define PIN1          A4
+// RFID reader SPI SS pin
+#define RF_PIN        10
+// SPI 10
+// SPI 11
+// SPI 12
+// RFID reset pin
+#define RF_RESET      11
+// Realy 1 pin
+#define RELAY1        A0
+// Relay 2 pin
+#define RELAY2        A1
+// OLED display I2C ID
+#define LCD_ID        0x3C
+// Maximum number of stored card slots
+#define CARD_MAX      5
+// Ignition macro. Just to make code clean.
+#define IGNITION      (digitalRead(RELAY1)==LOW)
+// Start macro
+#define START         start
+// Pin 1 state check macro
+#define PIN1_HIGH     (digitalRead(PIN1)==LOW)
+// Pin 2 state check macro
+#define PIN2_HIGH     (digitalRead(PIN2)==LOW)
+// Count fo retries to read card
+#define RETRY_MAX      5
+// Card storage EEPROM position
+#define EEPROM_CARDS  0
+// Display reset pin (not really used)
+#define OLED_RESET    4
+
 
 enum CARD { ABSENT, FAILURE, VALID, INVALID };// Card presence state constants
 
@@ -46,13 +66,13 @@ void setup() {
   display.begin(SSD1306_SWITCHCAPVCC, LCD_ID);// initialize with the I2C addr
   display.clearDisplay();                     // Clear the buffer.
   display.display();                          // Refreash diplay
-  pinMode(PIN1,   INPUT_PULLUP);                     // Set PIN1 gpio as input
-  pinMode(PIN2,   INPUT_PULLUP);                     // Set PIN2 gpio as input
+  pinMode(PIN1,   INPUT_PULLUP);              // Set PIN1 gpio as input
+  pinMode(PIN2,   INPUT_PULLUP);              // Set PIN2 gpio as input
   pinMode(RELAY1, OUTPUT);                    // Set RELAY1 gpio as output
   digitalWrite(RELAY1, HIGH);
   pinMode(RELAY2, OUTPUT);                    // Set RELAY2 gpio as output
   digitalWrite(RELAY2, HIGH);
-  // Shutdown engine. That is for that case if controller unexpactadly rebooted
+  // Shutdown engine. That is for that case if controller unexpectadly rebooted
   Serial.println("Shutdown engine");
   engineShutdown();                           // Shutdown engine
   ignitionStop();                             // Stop ignition
@@ -63,16 +83,17 @@ void setup() {
   Serial.println("Read stored cards");
   EEPROM_read(EEPROM_CARDS, cards);           // Read stored cards from EEPROM to RAM
   //while (cards[0] == 0 || PIN1_HIGH) {        // If first card slot is empty or PIN1 is HIGH waiting for new card to be presented
-  while (PIN1_HIGH) {        // If first card slot is empty or PIN1 is HIGH waiting for new card to be presented
+  while (PIN1_HIGH) {        // For debug
     Serial.println("Waiting for new card");
-    message("Present new card");
-    while (getCard() != INVALID) { delay(100); }
+    message("Present new card");              // Print message on screen
+    while (getCard() != INVALID) { delay(100); } // Waiting until card is prasented
     uint8_t i = 0;
     for (i = 0; i < CARD_MAX; i++) {          // Look for free slot to store card
       if (cards[i] == 0) {                    // if found empty slot
         cards[i] = getCard();                 // copy card ID to slot
         EEPROM_write(EEPROM_CARDS, cards);    // Store cards to EEPROM
-        message("Card added");
+        message("Card added");                // Print massage on screen
+        break;                                // Stop looking for free slot
       }
     }
     if (i >= CARD_MAX) {                      // No more slots to store cards
@@ -86,27 +107,25 @@ void setup() {
 
 // Main working cicle
 void loop() {
-  wdt_reset();                  // Reset watchdog timer
-  CARD state = cardState();
-  if (PIN1_HIGH) {
-    state = VALID;
-  }
-  if (state == VALID) {
-    cardTrys = 0;
-    if (! IGNITION) {
-      ignitionStart();
+  wdt_reset();                              // Reset watchdog timer
+  CARD state = cardState();                 // Try to read card. Possible states are VALID, INVALID, ABSENT, ERROR
+  if (PIN1_HIGH) state = VALID;             // For debug                       
+  if (state == VALID) {                     // Valid card is presented
+    cardTrys = 0;                           // Reset card read retry counter
+    if (! IGNITION) {                       // If Ignition is Off
+      ignitionStart();                      // turn it On
     }
     message("Ignition  on");
-  } else {
-    cardTrys++;
-    if (cardTrys > TRYS_MAX) {
-      if (START) {
-        engineShutdown();
+  } else {                                  // No valid card is presented
+    cardTrys++;                             // Increase retry counter
+    if (cardTrys > RETRY_MAX) {             // If retry counter exceed max
+      if (START) {                          // If engine is not in shutdown state
+        engineShutdown();                   // perform shutdown
       }
-      if (IGNITION) {
-        ignitionStop();
+      if (IGNITION) {                       // If Ignition is on
+        ignitionStop();                     // turn it Off
       }
-    switch (state) {
+    switch (state) {                        // Print message corresponding to current card presentation state
       case ABSENT:
         message("Waiting   for card...");
         break;
@@ -119,13 +138,15 @@ void loop() {
     }
     }
   }
-  if (START && PIN2_HIGH) {
-    engineShutdown();
+  if (START && PIN2_HIGH) {                 // If shutdown button is pressed
+    engineShutdown();                       // then shutdown engine
   }
-  delay(100);
+  delay(100);                               // Small delay to not overload peripherals with frequent queries
 }
 
+// Get PIIC UID. Using 4-bytes UID.
 uint32_t getCard() {
+  //Compose 32-bit value from 4 8-bit
   uint32_t curCard = mfrc522.uid.uidByte[0];
   curCard <<= 8;
   curCard = mfrc522.uid.uidByte[1];
@@ -135,34 +156,28 @@ uint32_t getCard() {
   curCard = mfrc522.uid.uidByte[3];
   return curCard;
 }
+// Get card presenrtation state
 CARD cardState() {
   // Getting ready for Reading PICCs
-  if ( ! mfrc522.PICC_IsNewCardPresent()) { //If a new PICC placed to RFID reader continue
-    return ABSENT;
+  if ( ! mfrc522.PICC_IsNewCardPresent()) { // If no card presented
+    return ABSENT;                          // Return ABSENT
   }
-  if ( ! mfrc522.PICC_ReadCardSerial()) {   //Since a PICC placed get Serial and continue
-    return FAILURE;
+  if ( ! mfrc522.PICC_ReadCardSerial()) {   // If unable to read card UID
+    return FAILURE;                         // return FAILURE
   }
-  // There are Mifare PICCs which have 4 byte or 7 byte UID care if you use 7 byte PICC
+  // There are Mifare PICCs which have 4 byte or 7 byte UID card if you use 7 byte PICC
   // I think we should assume every PICC as they have 4 byte UID
-  // Until we support 7 byte PICCs
   Serial.println(F("Scanned PICC's UID:"));
   for ( uint8_t i = 0; i < 4; i++) {
     Serial.print(mfrc522.uid.uidByte[i], HEX);
   }
   Serial.println("");
   //mfrc522.PICC_HaltA(); // Stop reading
-  uint32_t curCard = mfrc522.uid.uidByte[0];
-  curCard <<= 8;
-  curCard = mfrc522.uid.uidByte[1];
-  curCard <<= 8;
-  curCard = mfrc522.uid.uidByte[2];
-  curCard <<= 8;
-  curCard = mfrc522.uid.uidByte[3];
-  for (uint8_t i = 0; i < CARD_MAX; i++) {
-    if (cards[i] == curCard) return VALID;
+  uint32_t curCard = getCard();
+  for (uint8_t i = 0; i < CARD_MAX; i++) {  // Try to find card presented withng stoted cards
+    if (cards[i] == curCard) return VALID;  // Return VALID on success
   }
-  return INVALID;
+  return INVALID;                           // return INVALID otherway
 }
 
 void ignitionStart() {
@@ -187,7 +202,7 @@ void engineShutdown() {
 }
 void message(char* str) {
   display.clearDisplay();
-  display.setTextSize(2);
+  display.setTextSize(1);
   display.setTextColor(WHITE);
   display.setCursor(0,0);
   display.println(str);
